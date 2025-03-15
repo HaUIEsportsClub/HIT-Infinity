c370e1b15e3c888a9d7c445246fdcb24064a3498
Nguyễn Quyền Anh <qa662004@gmail.com>
2025-03-15 09:13:51 +0700

    base scrollbgr

---
+++ Assets/_Scripts/ScrollingBackground.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float scrollSpeed = 0.1f; 
+    private Material bgMaterial;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       
+
+    }
+}
