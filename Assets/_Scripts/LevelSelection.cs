2e512d95621af3fb66ee4cc888558d8eae3a2184
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2025-03-15 09:03:06 +0700

    level selection

--- Assets/_Scripts/LevelSelection.cs
+++ Assets/_Scripts/LevelSelection.cs
@@ -1,20 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    public Button[] levelButtons; 
+
+    private void Start()
     {
         
+        if (PlayerPrefs.GetInt("FirstRun", 1) == 1) 
+        {
+            ResetGameData();
+            PlayerPrefs.SetInt("FirstRun", 0); 
+            PlayerPrefs.Save();
+        }
+        UpdateLevelButtons();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void UpdateLevelButtons()
     {
-        
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            int levelIndex = i + 1; 
+            
+            
+            levelButtons[i].onClick.RemoveAllListeners();
+
+           
+            // if (LevelManager.Instance.IsLevelUnlocked(levelIndex))
+            // {
+            //     SetButtonUnlocked(i,levelIndex);
+            // }
+            // else SetButtonLocked(i);
+            
+        }
+    }
+
+    private void SetButtonUnlocked(int buttonIndex, int levelIndex)
+    {
+        levelButtons[buttonIndex].interactable = true; 
+        int capturedIndex = levelIndex;    
+        levelButtons[buttonIndex].onClick.AddListener(() => OnLevelButtonClick(capturedIndex));
+    }
+
+    private void SetButtonLocked(int buttonIndex)
+    {
+        levelButtons[buttonIndex].interactable = false;
+        levelButtons[buttonIndex].GetComponent<Image>().color = Color.gray; 
+    }
+
+   
+    private void OnLevelButtonClick(int level)
+    {
+        //LevelManager.Instance.LoadLevel(level);
+    }
+    public void ResetGameData()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+        Debug.Log("Game data has been reset.");
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
     }
 }
