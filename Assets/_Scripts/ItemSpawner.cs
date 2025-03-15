c0051eed954ef7e01430dd76765da2581a479ddc
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2025-03-15 08:37:26 +0700

    working with itemspawner

--- Assets/_Scripts/ItemSpawner.cs
+++ Assets/_Scripts/ItemSpawner.cs
@@ -6,6 +6,8 @@ public class ItemSpawner : MonoBehaviour
 {
     public static ItemSpawner Instance;
 
+    public Vector3 spawnPosition;
+
     public GameObject letterPrefab;
     public GameObject goldPrefab;
     public GameObject healthPrefab;
@@ -16,21 +18,35 @@ public class ItemSpawner : MonoBehaviour
     
     
     
-    // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnItemRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     IEnumerator SpawnItemRoutine()
     {
-        return null;
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(2, 3));
+            
+            float spawnRate = Random.Range(0, 1);
+            if (spawnRate < 0.2f) Instantiate(goldPrefab, spawnPosition, default);
+            if (spawnRate < 0.5f) SpawnLetter();
+            else Instantiate(healthPrefab, spawnPosition, default);
+        }
+    }
+
+    private void SpawnLetter()
+    {
+        float spawnTrueLetterRate = Random.Range(0, 1);
+        if (spawnTrueLetterRate < 0.2f)
+        {
+            char trueLetter = GameManager.Instance.currentAnswer[GameManager.Instance.currentLetterIndex];
+            Letter letterScript = Instantiate(letterPrefab, spawnPosition, default).GetComponent<Letter>();
+            //if(letterScript != null) letterSprites.SetL
+        }
     }
 
     public void SpawnHintLetter()
