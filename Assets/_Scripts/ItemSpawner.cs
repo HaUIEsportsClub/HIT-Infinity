4b98fd318765d47a902d4c645a2532a89a9d04fa
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2025-03-15 08:50:39 +0700

    item spawner

--- Assets/_Scripts/ItemSpawner.cs
+++ Assets/_Scripts/ItemSpawner.cs
@@ -1,56 +1,106 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class ItemSpawner : MonoBehaviour
 {
     public static ItemSpawner Instance;
+    [Header("Cài đặt Spawn")]
+    public GameObject letterPrefab;     
+    public GameObject healthPrefab;    
+    public GameObject goldPrefab;       
+    public Transform spawnPoint;       
 
-    public Vector3 spawnPosition;
+    [Header("Sprites chữ A-Z")]
+    public Sprite[] letterSprites;     
 
-    public GameObject letterPrefab;
-    public GameObject goldPrefab;
-    public GameObject healthPrefab;
-
-    public Sprite[] letterSprites;
-    
-    
-    
-    
-    
+    public float spawnIntervalMin = 2f;
+    public float spawnIntervalMax = 3f;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
     void Start()
     {
         StartCoroutine(SpawnItemRoutine());
     }
 
-    
-
     IEnumerator SpawnItemRoutine()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(2, 3));
+            yield return new WaitForSeconds(Random.Range(spawnIntervalMin, spawnIntervalMax));
+
+            //if (GameManager.Instance.gameActive == false)   yield break;
+
             
-            float spawnRate = Random.Range(0, 1);
-            if (spawnRate < 0.2f) Instantiate(goldPrefab, spawnPosition, default);
-            if (spawnRate < 0.5f) SpawnLetter();
-            else Instantiate(healthPrefab, spawnPosition, default);
+            float spawnRoll = Random.value; 
+
+            if (spawnRoll < 0.15f) Instantiate(healthPrefab, spawnPoint.position, Quaternion.identity);
+            else if (spawnRoll < 0.30f) Instantiate(goldPrefab, spawnPoint.position, Quaternion.identity);
+            
+            else SpawnLetterObject();
         }
     }
 
-    private void SpawnLetter()
+    private void SpawnLetterObject()
     {
-        float spawnTrueLetterRate = Random.Range(0, 1);
-        if (spawnTrueLetterRate < 0.2f)
+        bool spawnCorrect = (Random.value < 0.2f);
+        char letterToSpawn;
+        Sprite spriteToSpawn;
+
+                
+        char correctLetter = GameManager.Instance.currentAnswer[GameManager.Instance.currentLetterIndex];
+
+        if (spawnCorrect)
         {
-            char trueLetter = GameManager.Instance.currentAnswer[GameManager.Instance.currentLetterIndex];
-            Letter letterScript = Instantiate(letterPrefab, spawnPosition, default).GetComponent<Letter>();
-            //if(letterScript != null) letterSprites.SetL
+                    
+            letterToSpawn = correctLetter;
+            int index = letterToSpawn - 'A';
+            spriteToSpawn = letterSprites[index];
         }
-    }
+        else
+        {
+            int randomIndex;
+            do
+            {
+                randomIndex = Random.Range(0, letterSprites.Length);
+            } while ((char)('A' + randomIndex) == correctLetter);
 
-    public void SpawnHintLetter()
-    {
-        
+            letterToSpawn = (char)('A' + randomIndex);
+            spriteToSpawn = letterSprites[randomIndex];
+        }
+
+                
+        GameObject letterObj = Instantiate(letterPrefab, spawnPoint.position, Quaternion.identity);
+        Letter letterScript = letterObj.GetComponent<Letter>();
+
+        if (letterScript != null) letterScript.SetLetter(spriteToSpawn, letterToSpawn);
     }
+    public GameObject  SpawnHintLetter(char correctLetter)
+    {
+        int index = correctLetter - 'A'; 
+        if (index < 0 || index >= letterSprites.Length) return null;
+
+        Sprite correctSprite = letterSprites[index];
+
+        
+        GameObject letterObj = Instantiate(letterPrefab, spawnPoint.position, Quaternion.identity);
+        Letter letterScript = letterObj.GetComponent<Letter>();
+
+        if (letterScript != null)
+        {
+           
+            letterScript.SetLetter(correctSprite, correctLetter);
+        }
+        return letterObj;
+    }
+   
+
 }
